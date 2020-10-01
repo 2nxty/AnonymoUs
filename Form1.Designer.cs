3f9a4e564562f96aeed7f9d64e18ace6f631a4ca
Ruan991 <72177472+Ruan991@users.noreply.github.com>
2020-10-01 00:37:33 -0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AnonymoUs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cur_user = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AnonymoUs.Properties.Resources.among_us;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15F);
+            this.label1.Location = new System.Drawing.Point(72, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ANONYMO US";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(70, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "make me anonymous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 45);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "This tool makes your nickname invisible,\r\nleaving you completely anonymous.\r\n(Bet" +
+    "ter with friends)\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 30);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Enter your name as \"991\" and\r\nclose the game before clicking!";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cur_user
+            // 
+            this.cur_user.AutoSize = true;
+            this.cur_user.Location = new System.Drawing.Point(2, 183);
+            this.cur_user.Name = "cur_user";
+            this.cur_user.Size = new System.Drawing.Size(59, 15);
+            this.cur_user.TabIndex = 6;
+            this.cur_user.Text = "username";
+            this.cur_user.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.label2.FlatAppearance.BorderSize = 0;
+            this.label2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.label2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(274, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.UseVisualStyleBackColor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cur_user);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anonymo Us";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label cur_user;
+        private System.Windows.Forms.Button label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnonymoUs
+{
+    public partial class Form1 : Form
+    {
+        // Anonymo Us by Ruan991 on Github
+        // Open Source to learning
+
+        string usr;
+        private Point lastPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            usr = Environment.UserName; // set string usr current windows user
+            cur_user.Text = "User: " + usr; // hidden username textbox for debug
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string xname = "991"; // ingame name
+            string locat = "C:\\Users\\" + usr + "\\AppData\\LocalLow\\Innersloth\\Among Us\\playerPrefs"; // among us prefs location
+            if (File.ReadAllText(locat).Contains(xname)) // if ingame name = 991 (xname)
+            {
+                string text = File.ReadAllText(locat);
+                text = text.Replace(xname, "ㅤ"); // dont del the white space
+                File.WriteAllText(locat, text); // change 991 (xname) to invisible name
+                MessageBox.Show("Now you have no name!", "Anonymo Us", MessageBoxButtons.OK); // success message
+            }
+            else
+            {
+                MessageBox.Show("Have you changed your nickname to \"991\" and closed the game?", "error"); // error message if name doesnt match
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // X button
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/Ruan991"); // on icon click
+        }
+
+        // end
+
+        // move window code
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // end
+    }
+}
+
+
+// COME TO BRAZIL!!
